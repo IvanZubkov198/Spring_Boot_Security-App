969a3a777d35f83fa3521e2222665e4c4a2d8f11
Ivan Zubkov <zis.java.acc@gmail.com>
2024-07-14 06:58:25 +0300

    -

--- src/main/resources/insert_users.sql
+++ src/main/resources/insert_users.sql
@@ -1,9 +1,7 @@
--- INSERT INTO users VALUES (1, "user@user.com", "$2a$10$uKFJ0Z91cli2wjFmHguPGuCjZh8g3KH2enSpSMHOYtRGh52G8xehS", "user");
--- INSERT INTO users VALUES (2, "admin@admin.com", "$2a$10$IuyJ74kQHmthpEAm4QMqsuHyjJtNmgp8syUcOKEdRq6cM048rUi0.", "admin");
--- INSERT INTO roles VALUES (1, "ROLE_USER");
--- INSERT INTO roles VALUES (2, "ROLE_ADMIN");
--- INSERT INTO users_roles VALUES (1, 1);
--- INSERT INTO users_roles VALUES (2, 1);
--- INSERT INTO users_roles VALUES (2, 2);
+# INSERT INTO users VALUES (1, "user@user.com", "$2a$10$uKFJ0Z91cli2wjFmHguPGuCjZh8g3KH2enSpSMHOYtRGh52G8xehS", "user"),
+#                          (2, "admin@admin.com", "$2a$10$IuyJ74kQHmthpEAm4QMqsuHyjJtNmgp8syUcOKEdRq6cM048rUi0.", "admin");
+# INSERT INTO roles VALUES (1, "ROLE_USER"),(2, "ROLE_ADMIN");
+# INSERT INTO users_roles VALUES (1, 1),(2,1),(2,2);
+
 
 # Password for users equals his usernames
